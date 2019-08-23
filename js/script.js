dea0de38e44b8c220b86d6e1ef5649e97653a245
Brendon Tse <brendonjtse@gmail.com>
2019-08-23 16:23:47 -0700

    add final else if statement

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ for (var i=0; i<=userInput; i++) {
       resultsArray.push("Beep!");
     } else if (i.toString().includes('2')) {
       resultsArray.push('Boop!');
+    } else if (i.toString().includes('3')) {
+        resultsArray.push("I'm sorry, Dave. I'm afraid I can't do that.");
     } else {
       resultsArray.push(i);
     }
