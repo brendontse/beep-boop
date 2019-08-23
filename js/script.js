93cb2b6f176a967007762d7173f07ac626cded80
Brendon Tse <brendonjtse@gmail.com>
2019-08-23 14:07:11 -0700

    define variables for backend logic

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+var beepboop = function(userInput) {
+var resultsArray = [];
+
+
+
 $(document).ready(function() {
   $('form#beepboop').submit(function(event) {
     event.preventDefault();
@@ -5,3 +10,5 @@ $(document).ready(function() {
     var userNumber = parseInt($('input#userInputNumber').val());  // changes user input from a string to a number, assigns it to var userNumber
     var userOutput = numberToOutput(userNumber);    // calls function on userNumber, assigns it to var userOutput
     $('#userResult').show();
+  });
+});
