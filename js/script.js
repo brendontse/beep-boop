146d65afa48fc07aa16519429a0a938fe751c042
Brendon Tse <brendonjtse@gmail.com>
2019-08-23 12:34:07 -0700

    add div for user results and javascript to show results

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $('form#beepboop').submit(function(event) {
+    event.preventDefault();
+
+    var userNumber = parseInt($('input#userInputNumber').val());  // changes user input from a string to a number, assigns it to var userNumber
+    var userOutput = numberToOutput(userNumber);    // calls function on userNumber, assigns it to var userOutput
+    $('#userResult').show();
