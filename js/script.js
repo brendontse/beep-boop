d7ee841087d35ab86deedd891f9bf4e05999ecc4
Brendon Tse <brendonjtse@gmail.com>
2019-08-23 16:32:00 -0700

    fix indentation in scripts.js and index.html

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 var beepboop = function(userInput) {
-var resultsArray = [];
+  var resultsArray = [];
 
-for (var i=0; i<=userInput; i++) {
+  for (var i = 0; i <= userInput; i++) {
     if (i.toString().includes('1')) {
       resultsArray.push("Beep!");
     } else if (i.toString().includes('2')) {
       resultsArray.push('Boop!');
     } else if (i.toString().includes('3')) {
-        resultsArray.push("I'm sorry, Dave. I'm afraid I can't do that.");
+      resultsArray.push("I'm sorry, Dave. I'm afraid I can't do that.");
     } else {
       resultsArray.push(i);
     }
@@ -20,9 +20,9 @@ $(document).ready(function() {
   $('form#beepboop').submit(function(event) {
     event.preventDefault();
 
-    var userNumber = parseInt($('input#userInputNumber').val());  // changes user input from a string to a number, assigns it to var userNumber
-    var userOutput = beepboop(userNumber);    // calls function on userNumber, assigns it to var userOutput
+    var userNumber = parseInt($('input#userInputNumber').val()); // changes user input from a string to a number, assigns it to var userNumber
+    var userOutput = beepboop(userNumber); // calls function on userNumber, assigns it to var userOutput
     $('#userResult').show();
-});
+  });
 
 });
